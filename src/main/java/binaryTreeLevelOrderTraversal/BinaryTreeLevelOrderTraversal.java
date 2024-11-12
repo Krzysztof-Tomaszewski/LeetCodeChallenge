bc83e318038309e0e3179aed7ce86eb045f963a4
Krzysztof Tomaszewski <krzysztof.tom@outlook.com>
2024-11-12 07:58:12 +0100

    Added solution to Binary Tree Level Order Traversal

--- src/main/java/binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/binaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java
@@ -13,13 +13,68 @@ package binaryTreeLevelOrderTraversal;
 // Output: [[3],[9,20],[15,7]]
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class BinaryTreeLevelOrderTraversal {
     // Method to implement
     List<List<Integer>> levelOrder(TreeNode root) {
         // Your solution goes here
-        return new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<Pair<Integer, TreeNode>> queue = new LinkedList<>();
+
+
+        if (root == null) return result;
+        result.add(List.of(root.val));
+        if (root.left != null) {
+            queue.offer(new Pair<>(1, root.left));
+        }
+
+        if (root.right != null) {
+            queue.offer(new Pair<>(1, root.right));
+        }
+
+
+        while (!queue.isEmpty()) {
+            Pair<Integer, TreeNode> pair = queue.poll();
+            int currentLevel = pair.first();
+            TreeNode currentNode = pair.second();
+
+            if (result.size() <= currentLevel) {
+                result.add(new ArrayList<>());
+            }
+            result.get(currentLevel).add(currentNode.val);
+
+
+            if (currentNode.left != null) {
+                queue.offer(new Pair<>(currentLevel + 1, currentNode.left));
+            }
+
+            if (currentNode.right != null) {
+                queue.offer(new Pair<>(currentLevel + 1, currentNode.right));
+            }
+
+        }
+        return result;
+    }
+
+    record Pair<T, U>(T first, U second) {
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair<?, ?> pair = (Pair<?, ?>) o;
+            return first.equals(pair.first) && second.equals(pair.second);
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "first=" + first +
+                    ", second=" + second +
+                    '}';
+        }
     }
 }
-
